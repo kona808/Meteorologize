abe8099109354aa0f814f4fc64b522e23ec52df4
Jacob I  Perez <jacobperez91@gmail.com>
2023-02-08 20:37:52 -1000

    Created a new city method

---
+++ Meterologize/Meterologize/Controllers/Model Controller/CityController.swift
@@ -0,0 +1,27 @@
+//
+//  CityController.swift
+//  Meterologize
+//
+//  Created by Jacob Perez on 2/8/23.
+//
+
+import Foundation
+
+class CityController {
+    
+    // MARK:- Properties
+    var cities: [City] = []
+    
+    
+    // What do we need to create a City? Anything that doesn't have a defauly value.
+    // tell the parameters what to expect.. What information am I going to need?
+    func createCity(name: String, currentTemp: Double, dailyHigh: Int, dailyLow: Int, currentStatus: String) {
+        // How are we going to make a City from these parameters?
+        // Call a function we've made
+        let newCity = City(name: name, currentTemp: currentTemp, dailyHigh: dailyHigh, dailyLow: dailyLow, currentStatus: currentStatus)
+        // How to get a new City onto the cities array?
+        cities.append(newCity)
+        
+        
+    }
+} //End of class
---
+++ Meterologize/Meterologize/Model/City.swift
@@ -0,0 +1,25 @@
+//
+//  City.swift
+//  Meterologize
+//
+//  Created by Jacob Perez on 2/8/23.
+//
+
+import Foundation
+
+class City {
+    
+    let name: String
+    let currentTemp: Double
+    let dailyHigh: Int
+    let dailyLow: Int
+    let currentStatus: String
+    
+    init(name: String, currentTemp: Double, dailyHigh: Int, dailyLow: Int, currentStatus: String) {
+        self.name = name
+        self.currentTemp = currentTemp
+        self.dailyHigh = dailyHigh
+        self.dailyLow = dailyLow
+        self.currentStatus = currentStatus
+    }
+} //End of class
